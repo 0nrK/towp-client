e20ea9f280d22632a1c769d25471a12780f9b0ba
0nrk <onurkirnapci@gmail.com>
2023-04-23 18:27:56 +0300

    chore: remove webpack-node-externals

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
-import nodeExternals from "webpack-node-externals";
-
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   images: {
-    domains: ["img.youtube.com", "img.ytimg.com"],
+    domains: ["img.youtube.com", "img.ytimg.com" ],
   },
 };
 
-module.exports = withBundleAnalyzer({
-  webpack(nextConfig) {
-    config.externals = [nodeExternals()];
-    return config;
-  },
-});
+module.exports = nextConfig;
