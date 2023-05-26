55aaf89db58cbd4c61d8ddac640283332f08dd1c
0nrk <onurkirnapci@gmail.com>
2023-05-26 23:27:18 +0300

    fix: chat scroll problem

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -9,32 +9,41 @@ const Chat = () => {
   const [messages, setMessages] = useState<IMessage[]>();
   const chatRef = useRef<HTMLDivElement>(null);
 
+
   useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight + 500;
+    }
     socket.on("GET_MESSAGES", (data: any) => {
-      if (chatRef.current) {
-        chatRef.current.scrollTop = chatRef.current.scrollHeight;
-      }
-      setMessages(data);
+      setMessages(() => data);
     });
     socket.on('USER_BANNED', (data: any) => {
       setMessages((prevMessages) => [...prevMessages as [], { user: 'System', message: data }])
     })
   }, []);
 
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight + 500;
+    }
+  }, [messages]);
+
+  if (!messages) return <p className="text-2xl text-white"> Loading...</p>
+
   return (
-    <div className="w-full md:w-96 relative rounded-lg  overflow-y-hidden  h-96  bg-slate-800">
+    <section className="w-full md:w-96 max-h-[24rem]">
       <div
+        className=" rounded-lg  overflow-y-scroll max-h-[20rem]  h-full scroll-smooth  bg-slate-800"
         ref={chatRef}
-        className=" space-y-1 h-5/6  overflow-y-scroll mb-3 scroll-smooth"
       >
         {messages?.map((item, index) => {
           return <Message key={index} message={item.message} user={item.user} />;
         })}
       </div>
-      <div className="h-full mt-4">
+      <div className="h-[4rem] mt-4">
         <Input />
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(`${process.env.NEXT_PUBLIC_SOCKET_URL}`, {
+export const socket = io(`wss://${process.env.NEXT_PUBLIC_SOCKET_URL}`, {
     transports: ['websocket'],
     path: '/socket.io'
 });
